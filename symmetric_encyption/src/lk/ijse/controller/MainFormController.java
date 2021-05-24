6c5dfead867a14c27fead2e3dd40b6ea3e6c2d37
Manoj Randeni <randeni.ramr@gmail.com>
2021-05-24 10:49:27 +0530

    Create MainFormController methods

--- symmetric_encyption/src/lk/ijse/controller/MainFormController.java
+++ symmetric_encyption/src/lk/ijse/controller/MainFormController.java
@@ -1,19 +1,41 @@
 package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainFormController {
     public Button btnEncryptForm;
     public Button btnDecryptForm;
 
-    public void btnEncryptForm_onAction(ActionEvent actionEvent) {
+    public void btnEncryptForm_onAction(ActionEvent actionEvent) throws IOException {
+        formLoad("/lk/ijse/view/EncryptForm.fxml", "Encryption");
     }
 
-    public void btnDecryptForm_onAction(ActionEvent actionEvent) {
+    public void btnDecryptForm_onAction(ActionEvent actionEvent) throws IOException {
+        formLoad("/lk/ijse/view/DecryptForm.fxml", "Decryption");
+
     }
 
-    public void formLoad(String location){
-
+    public void formLoad(String location, String title) throws IOException {
+        URL root = getClass().getResource(location);
+        Parent load = FXMLLoader.load(root);
+        Stage stage = new Stage();
+        Scene scene = new Scene(load);
+        stage.setScene(scene);
+        stage.setTitle(title);
+        stage.centerOnScreen();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(
+                (btnDecryptForm.getScene().getWindow())
+        );
+        stage.show();
     }
 }
