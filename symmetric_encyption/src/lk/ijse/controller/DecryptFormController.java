520a5dc03081f0ec554d20bcfc1e7f9dba9ba740
Manoj Randeni <randeni.ramr@gmail.com>
2021-05-25 03:38:25 +0530

    Create cypher

--- symmetric_encyption/src/lk/ijse/controller/DecryptFormController.java
+++ symmetric_encyption/src/lk/ijse/controller/DecryptFormController.java
@@ -2,21 +2,44 @@ package lk.ijse.controller;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-
+import javafx.scene.control.*;
+import javafx.scene.input.Clipboard;
+import lk.ijse.crypto.DEP7Crypto;
 
 public class DecryptFormController {
     public Button btnEncrypt;
     public TextArea txtToDecrypt;
-    public TextArea txtDecryptedText;
     public TextField txtEncryptKey;
+    public TextArea txtOriginal;
+    public Button btnPasteCypher;
 
     public void initialize() {
-        Platform.runLater(()->txtToDecrypt.requestFocus());
+        Platform.runLater(() -> txtToDecrypt.requestFocus());
     }
 
     public void btnEncrypt_onAction(ActionEvent actionEvent) {
+        String cipherText = txtToDecrypt.getText().trim();
+        String key = txtEncryptKey.getText().trim();
+
+        if (cipherText.isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Please enter valid text to encrypt", ButtonType.OK).show();
+            txtToDecrypt.requestFocus();
+            return;
+        }
+
+        if (key.isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Please enter valid key", ButtonType.OK).show();
+            txtEncryptKey.requestFocus();
+            return;
+        }
+
+        txtOriginal.setText(DEP7Crypto.decrypt(cipherText, key));
+
+    }
+
+    // Paste btn
+    public void btnPasteCypher_onAction(ActionEvent actionEvent) {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        txtToDecrypt.setText(clipboard.getString());
     }
 }
--- symmetric_encyption/src/lk/ijse/controller/EncryptFormController.java
+++ symmetric_encyption/src/lk/ijse/controller/EncryptFormController.java
@@ -3,13 +3,17 @@ package lk.ijse.controller;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import lk.ijse.crypto.DEP7Crypto;
 
 
 public class EncryptFormController {
     public Button btnEncrypt;
     public TextArea txtToEncrypt;
     public TextField txtEncryptKey;
-    public TextArea txtEncryptedText;
+    public TextArea txtCypher;
+    public Button btnCopyCypher;
 
     public void initialize() {
         Platform.runLater(() -> txtToEncrypt.requestFocus());
@@ -18,37 +22,29 @@ public class EncryptFormController {
 
     public void btnEncrypt_onAction(ActionEvent actionEvent) {
 
-        String text = txtToEncrypt.getText() == null ? "" : txtToEncrypt.getText();
-        String key = txtEncryptKey.getText() == null ? "" : txtEncryptKey.getText();
+        String text = txtToEncrypt.getText().trim();
+        String key = txtEncryptKey.getText().trim();
 
-        if (text.trim().isEmpty()) {
+        if (text.isEmpty()) {
             new Alert(Alert.AlertType.ERROR, "Please enter valid text to encrypt", ButtonType.OK).show();
             txtToEncrypt.requestFocus();
             return;
         }
 
-        if (key.trim().isEmpty()) {
+        if (key.isEmpty()) {
             new Alert(Alert.AlertType.ERROR, "Please enter valid key", ButtonType.OK).show();
             txtEncryptKey.requestFocus();
             return;
         }
 
-        text += key;
-        String reversedText = "";
-        int textLength = text.length();
-        for (int i = textLength - 1; i >= 0; i--) {
-            reversedText += text.charAt(i);
-        }
+        txtCypher.setText(DEP7Crypto.encrypt(text, key));
+    }
 
-        String cipherText = "";
-        int reversedTextLength = reversedText.length();
-        for (int i = 0; i < reversedTextLength; i++) {
-            int charCode = reversedText.charAt(i);
-            charCode += 10;
-            char newChar = (char) charCode;
-            cipherText += newChar;
-        }
-
-        txtEncryptedText.setText(cipherText);
+    // Copy btn
+    public void btnCopyCypher_onAction(ActionEvent actionEvent) {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent content = new ClipboardContent();
+        content.putString(txtCypher.getText());
+        clipboard.setContent(content);
     }
 }
